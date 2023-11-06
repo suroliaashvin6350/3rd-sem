cbfcb517ff19ee767a78513225f37dc1168c1eba
surolia ashvin <suroliaashvin56@gmail.com>
2023-11-06 12:21:13 +0530

    first

---
+++ PYTHON1/task.py
@@ -0,0 +1,20 @@
+print(2+3)
+print(5+8)
+print(29)
+#() is called paranthesis
+print("patna")
+print("patna")
+a=9
+b=5.7
+z="hello"
+print(a,b,z)
+
+#printing data types in variables
+print(type(a))
+print(type(b))
+print(type(z))
+
+#maths operation
+sum1 = a+b
+mul1 = a*b
+print(sum1,mul1)   
---
+++ PYTHON1/task1.py
@@ -0,0 +1,6 @@
+n1 = input("enter first number:")
+# by default input takes all argument as string
+n2= input ("enter second number:")
+# now change data type to int type casting
+sum1= int(n1) + int(n2)
+print("the sum of these two numbers is",sum1)
